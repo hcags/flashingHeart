7728befda9441bb384c12581382ac2a06bd94fe5
hcags <120849679+hcags@users.noreply.github.com>
2022-12-21 15:12:20 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+basic.showLeds(`
+    . # . # .
+    # # # # #
+    # # # # #
+    . # # # .
+    . . # . .
+    `)
+let ShaowLed = 1
 basic.forever(function () {
-	
+    if (ShaowLed == 0) {
+        led.enable(true)
+        ShaowLed = 1
+    } else {
+        led.enable(false)
+        ShaowLed = 0
+    }
+    basic.pause(500)
 })
