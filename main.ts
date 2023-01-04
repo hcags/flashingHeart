565e6b829f36c1a5c6ebfdf5e964fa18aa62c07d
hcags <120849679+hcags@users.noreply.github.com>
2023-01-04 14:57:26 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,14 @@
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+	
+})
 basic.showLeds(`
     . # . # .
-    # # # # #
+    # # . # #
     # # # # #
     . # # # .
     . . # . .
     `)
 let ShowLed = 1
 basic.forever(function () {
-    if (ShowLed == 0) {
-        led.enable(true)
-        ShowLed = 1
-    } else {
-        led.enable(false)
-        ShowLed = 0
-    }
-    basic.pause(500)
+	
 })
