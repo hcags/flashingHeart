052ea061cb5d0b1335ebd789bdeb90a09d00aa22
hcags <120849679+hcags@users.noreply.github.com>
2023-01-11 05:51:56 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,16 +8,16 @@ basic.showLeds(`
     . # # # .
     . . # . .
     `)
-let ShowLed = 1
+let showLed = 1
 basic.forever(function () {
     if (true) {
     	
-    } else if (ShowLed == 0) {
+    } else if (showLed == 0) {
         led.enable(true)
-        ShowLed = 1
-    } else if (ShowLed == 1) {
+        showLed = 1
+    } else if (showLed == 1) {
         led.enable(false)
-        ShowLed = 0
+        showLed = 0
     }
     basic.pause(500)
 })
