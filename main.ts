dab0294db639f7fdcd3582ab5b96b933c357ed28
hcags <120849679+hcags@users.noreply.github.com>
2023-01-11 05:31:45 +0100

    Geschüttelt entfernt, wenn-dann abfrage gekürzt

--- main.ts
+++ main.ts
@@ -12,38 +12,25 @@ function changeSpeed (limit: number, speedStep: number) {
         speed += speedStep
     }
 }
-input.onGesture(Gesture.Shake, function () {
-    working = false
-    basic.setLedColor(0xffff00)
-    led.setBrightness(255)
-    basic.showString("" + (speed))
-    basic.showIcon(IconNames.Heart)
-    working = true
-})
 let speed = 0
 let working = false
+let icon = 0
 let brightness = 255
 let steps = 10
-let icon:number = 0
-let icons = {
-    0:IconNames.Heart,
-    1:IconNames.Happy,
-    2:IconNames.Duck,
-    3:IconNames.Skull}
-basic.showIcon(icons[icon])
+working = true
+basic.showIcon(IconNames.Heart)
 basic.setLedColor(0x00ff00)
 basic.forever(function () {
-    if (working) {
-        basic.setLedColor(0x00ff00)
-        if (brightness <= 0) {
-            steps = 10
-        } else if (brightness >= 255) {
-            steps = -10
-        }
-        brightness += steps
-        led.setBrightness(brightness)
-    } else {
+    if (!(working)) {
         basic.setLedColor(0xff0000)
+        return
+    } else if (brightness <= 0) {
+        steps = 10
+    } else if (brightness >= 255) {
+        steps = -10
     }
+    brightness += steps
+    led.setBrightness(brightness)
+    basic.setLedColor(0x00ff00)
     basic.pause(speed)
 })
